b204942e5aa3cd4057f6bdb5221cdd49a889932c
Wong Keng <kaira428@gmail.com>
2023-03-09 18:44:47 +0800

    useSearchParams hook

--- src/BookLayout.js
+++ src/BookLayout.js
@@ -1,16 +1,27 @@
-import { Link, Outlet, useParams } from "react-router-dom";
+import { Link, Outlet, useParams, useSearchParams } from "react-router-dom";
 
 const BookLayout = () => {
-    const {bookId} = useParams()
+  const { bookId } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams({n: 3})
+
+  const number = searchParams.get("n");
+
   return (
     <>
       <Link to="/books/1">Book 1</Link>
       <br />
       <Link to="/books/2">Book 2</Link>
       <br />
+      <Link to={`/books/${number}`}>Book {number}</Link>
+      <br />
       <Link to="/books/new">Create New Book</Link>
-      <Outlet context={{hello: "World", book: bookId}}/>
-    </>    
+      <Outlet context={{ hello: "World", book: bookId }} />
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setSearchParams({n: e.target.value})}
+      />
+    </>
   );
 };
 
